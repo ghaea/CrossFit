1d90d86165ee078a8ac3ea3f1ce6b75bf346b57c
Ghaea <ghaea.tam@gmail.com>
2015-11-17 18:38:52 -0500

    added find trainer, start, and libs folder

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function() {
+	$('.menuButton').on('click', function() {
+		$('.menu').toggleClass('active')
+	})
+
+	$('.find').on('click', function() {
+		$('.findATrainer').toggleClass('hidden')
+		$('.menuButton').hide()
+		$('.menu').toggleClass('active')
+
+	})
+
+	$('.back').on('click', function() {
+		$('.findATrainer').toggleClass('hidden')
+		$('.menuButton').show()
+		$('.menu').toggleClass('active')
+	})
+})
